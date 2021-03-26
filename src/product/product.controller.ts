25c7931d0481ed3abb649cc125edc1a79cf26992
Ali Abbasi <Ali@localhost.localdomain>
2021-03-26 20:33:58 +0430

    feature: add 2 end point create and see product and add mongo

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateProductDto } from './models/product.dto';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+
+    constructor(private readonly productService: ProductService){}
+
+    @Post()
+    createProduct(@Body() createProductDto: CreateProductDto){
+        this.productService.createProduct(createProductDto)
+    }
+
+    @Get()
+    getAllProduct(){
+        return this.productService.getAllProduct()
+    }
+}
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { Product, ProductSchema } from './schemas/product.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: Product.name, schema: ProductSchema}])],
   controllers: [ProductController],
   providers: [ProductService]
 })

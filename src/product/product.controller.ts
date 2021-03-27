82d856651679c9338482af6eaeeca11cb1b25d49
Ali Abbasi <Ali@localhost.localdomain>
2021-03-27 13:37:01 +0430

    Feature: add authentication and add owner to product and correct response

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,7 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/shared/auth/jwt.guard';
+import { User } from 'src/shared/auth/user.decorator' 
 import { Result } from 'src/shared/main.helper';
 import { CreateProductDto } from './models/product.dto';
 import { ProductService } from './product.service';
@@ -9,11 +12,15 @@ export class ProductController {
     constructor(private readonly productService: ProductService){}
 
     @Post()
-    createProduct(@Body() createProductDto: CreateProductDto): Promise<Result>{
-        return this.productService.createProduct(createProductDto)
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    createProduct(@User() user,@Body() createProductDto: CreateProductDto): Promise<Result>{       
+        return this.productService.createProduct(createProductDto, user.id)
     }
 
     @Get()
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
     getAllProduct(@Query('count') count: number, @Query('page') page: number){
         return this.productService.getAllProduct(page,count)
     }
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtStrategy } from 'src/shared/auth/jwt.strategy';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { Product, ProductSchema } from './schemas/product.schema';
@@ -7,6 +8,6 @@ import { Product, ProductSchema } from './schemas/product.schema';
 @Module({
   imports: [MongooseModule.forFeature([{name: Product.name, schema: ProductSchema}])],
   controllers: [ProductController],
-  providers: [ProductService]
+  providers: [ProductService, JwtStrategy]
 })
 export class ProductModule {}
